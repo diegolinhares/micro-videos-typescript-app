7ef53c01d02082f6c38f0ab6d7ca609d157ee70e
Diego Linhares <diegomlinhares@gmail.com>
2023-05-10 23:35:41 -0300

    Update test to use props

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,8 +2,27 @@ import { Category } from "./category"
 
 describe("Category Tests", () => {
   test("constructor of category", () => {
-    const category = new Category("Movie")
+    // Arrange
+    const created_at = new Date
+
+    const props = {
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at: created_at
+    }
+
+    // Act
+    const category = new Category(props)
+
+    // Assert
+    expect(category.props).toStrictEqual(props)
+
+    expect(category.props).toMatchObject(props)
 
     expect(category.name).toBe("Movie")
+    expect(category.description).toBe("some description")
+    expect(category.is_active).toBeTruthy()
+    expect(category.created_at).toBe(created_at)
   })
 })
