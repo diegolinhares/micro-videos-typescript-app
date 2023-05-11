9dbcc5f580e9c4a904a47d63743fdca59956b045
Diego Linhares <diegomlinhares@gmail.com>
2023-05-11 18:38:27 -0300

    Update test to better readability

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -3,15 +3,14 @@ import UniqueEntityId from "./unique-entity-id.vo"
 import {validate as uuidValidate} from "uuid"
 
 describe("UniqueEntityId Unit Tests", () => {
-  it("should throw error when UUID is invalid", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate")
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate")
 
+  it("should throw error when UUID is invalid", () => {
     expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError())
     expect(validateSpy).toHaveBeenCalled()
   })
 
   it("should accept a uuid passed in constructor", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate")
     const uuid = "7b0868b3-ad35-413b-b1e4-ba93e83d2e73"
     const valueObject = new UniqueEntityId(uuid)
 
@@ -20,7 +19,6 @@ describe("UniqueEntityId Unit Tests", () => {
   })
 
   it("should accept a uuid passed in constructor", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate")
     const valueObject = new UniqueEntityId()
 
     expect(uuidValidate(valueObject.id)).toBeTruthy()
