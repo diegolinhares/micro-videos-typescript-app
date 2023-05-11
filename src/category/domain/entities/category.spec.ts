743073eda1ba9d03b6ec7338b0b5a21d524897ed
Diego Linhares <diegomlinhares@gmail.com>
2023-05-11 00:36:29 -0300

    Add setters for category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,28 +1,56 @@
 import { Category } from "./category"
+import { create, omit } from "lodash"
 
 describe("Category Tests", () => {
   test("constructor of category", () => {
-    // Arrange
-    const created_at = new Date
+    let category = new Category({name: "Movie"})
+    let props = omit(category.props, "created_at")
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
+      is_active: true
+    })
+    expect(category.props.created_at).toBeInstanceOf(Date)
 
-    const props = {
+    category = new Category({
       name: "Movie",
       description: "some description",
-      is_active: true,
-      created_at: created_at
-    }
+      is_active: false
+    })
+    let created_at = new Date()
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+      created_at
+    })
 
-    // Act
-    const category = new Category(props)
+    category = new Category({
+      name: "Movie",
+      description: "other description"
+    })
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description"
+    })
 
-    // Assert
-    expect(category.props).toStrictEqual(props)
+    category = new Category({
+      name: "Movie",
+      is_active: true
+    })
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true
+    })
 
-    expect(category.props).toMatchObject(props)
-
-    expect(category.name).toBe("Movie")
-    expect(category.description).toBe("some description")
-    expect(category.is_active).toBeTruthy()
-    expect(category.created_at).toBe(created_at)
+    created_at = new Date()
+    category = new Category({
+      name: "Movie",
+      created_at
+    })
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at
+    })
   })
 })
