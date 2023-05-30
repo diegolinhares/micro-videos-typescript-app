40abdc7f4e6a732a522c6d361d8e6fc9c5d04646
Diego Linhares <diegomlinhares@gmail.com>
2023-05-30 20:59:41 -0300

    Add searchable interface repository

--- src/@seedwork/domain/repository/repository-contracts.ts
+++ src/@seedwork/domain/repository/repository-contracts.ts
@@ -1,5 +1,6 @@
 import Entity from "../entity/entity"
 import UniqueEntityId from "../value-objects/unique-entity-id.vo"
+import { InMemoryRepository } from "./in-memory.repository"
 
 export interface RepositoryInterface<E extends Entity> {
   insert(entity: E): Promise<void>
@@ -7,4 +8,21 @@ export interface RepositoryInterface<E extends Entity> {
   findAll(): Promise<E[]>
   update(entity: E): Promise<void>
   delete(id: string | UniqueEntityId): Promise<void>
+}
+
+export interface SearchableRepositoryInterface<
+  E extends Entity,
+  SearchParams,
+  SearchResult
+> extends RepositoryInterface<E> {
+  search(props: SearchParams): Promise<SearchResult>
+}
+
+export abstract class InMemorySearchableRepository<E extends Entity>
+  extends InMemoryRepository<E>
+  implements SearchableRepositoryInterface<E, any, any> {
+
+  search(props: any): Promise<any> {
+    throw new Error("Method not implemented.")
+  }
 }
