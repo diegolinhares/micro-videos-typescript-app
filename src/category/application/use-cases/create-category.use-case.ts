08cbc734c20a871ab2fc6c031bda17f7126719cd
Diego Linhares <diegomlinhares@gmail.com>
2023-08-12 17:53:27 -0300

    Add list categories use case

--- src/category/application/use-cases/create-category.use-case.ts
+++ src/category/application/use-cases/create-category.use-case.ts
@@ -1,7 +1,7 @@
 import UseCase from "../../../@seedwork/application/use-case"
 import CategoryRepository from "../../domain/repository/category.repository"
 import { Category } from "../../domain/entities/category"
-import { CategoryOutput } from "../dto/category-output.dto"
+import { CategoryOutput, CategoryOutputMapper } from "../dto/category-output"
 
 export default class CreateCategoryUseCase implements UseCase<Input, Output>{
   constructor(private categoryRepo: CategoryRepository.Repository) {
@@ -10,13 +10,7 @@ export default class CreateCategoryUseCase implements UseCase<Input, Output>{
   async execute(input: Input): Promise<Output> {
     const entity = new Category(input)
     await this.categoryRepo.insert(entity)
-    return {
-      id: entity.id,
-      name: entity.name,
-      description: entity.description,
-      is_active: entity.is_active,
-      created_at: entity.created_at
-    }
+    return CategoryOutputMapper.toOutput(entity)
   }
 }
 
