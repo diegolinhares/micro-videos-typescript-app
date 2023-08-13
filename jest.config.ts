60471024c86071d2af867b6ee7e5fc31440ba580
Diego Linhares <diegomlinhares@gmail.com>
2023-08-13 01:51:34 +0000

    Add coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,14 @@
 export default {
   clearMocks: true,
+  coverageDirectory: "<rootDir>/../__coverage",
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    },
+  },
   coverageProvider: "v8",
   rootDir: "src",
   testRegex: ".*\\..*spec\\.ts$",
